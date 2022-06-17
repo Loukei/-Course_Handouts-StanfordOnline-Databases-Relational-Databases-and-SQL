de7953d9073ec73ceca31f5ab69cb68314595eed
Loukei <weber781015+bitbucket@gmail.com>
2022-06-17 09:58:25 +0800

    Exercise - Social network Q3,Q4

--- Quiz&Homeworks/SQL Social-Network Query Exercises Level/Q3.sql
+++ Quiz&Homeworks/SQL Social-Network Query Exercises Level/Q3.sql
@@ -6,6 +6,13 @@ Include each pair only once, with the two names in alphabetical order.
 
 /*
 Students likes each other
+
+result
+id1		id2
+1689	1709
+1709	1689
+1501	1934
+1934	1501
 */
 -- SELECT L1.ID1, L1.ID2
 -- FROM Likes L1
@@ -15,11 +22,11 @@ Students likes each other
 /*
 USE the sudents like each other, get the name and grade
 
-問題
-1: 要求不能重複，即a,b = b,a
-2: 最後的結果是依照姓名排序而非id，也就是說在排序之前不能事先刪除重複的，否則會導致排序出錯
+- 前一階段可以拿到的是重覆的ID，即{(a,b),(b,a),(c,d),(d,c)}這種
+- 為了刪除重複的值，依照題目要求必須以姓名排序，因此使用where子句過濾
+- 為了保留(a,b)並除掉(b,a)的選項，一個簡單的做法是依照name1 < name2
 */
-SELECT H1.ID, H1.name, H1.grade, H2.ID, H2.name, H2.grade
+SELECT H1.name AS 'Name_1', H1.grade, H2.name AS 'Name_2', H2.grade
 FROM (
 	SELECT L1.ID1, L1.ID2
 	FROM Likes L1
@@ -30,4 +37,4 @@ INNER JOIN Highschooler H1
 ON H1.ID = L3.ID1
 INNER JOIN Highschooler H2
 ON H2.ID = L3.ID2
-ORDER BY H1.name;
+WHERE H1.name < H2.name; ---trick
--- Quiz&Homeworks/SQL Social-Network Query Exercises Level/Q4.sql
+++ Quiz&Homeworks/SQL Social-Network Query Exercises Level/Q4.sql
@@ -1,4 +1,26 @@
 /*
 ## QUESTION
-
+Find all students who do not appear in the Likes table (as a student who likes or is liked) and return their names and grades. 
+Sort by grade, then by name within each grade.
 */
+
+/*
+ANS 1
+*/
+SELECT H.name, H.grade
+FROM Highschooler H
+WHERE H.ID NOT IN ( SELECT ID1 AS 'ID' FROM Likes UNION SELECT ID2 AS 'ID' FROM Likes )
+ORDER BY H.grade; 
+
+/*
+ANS 2
+*/
+SELECT H.name, H.grade
+FROM Highschooler H
+LEFT JOIN (
+	SELECT ID1 AS ID FROM Likes
+	UNION
+	SELECT ID2 AS ID FROM Likes
+) AS T USING(ID)
+WHERE T.ID IS NULL
+ORDER BY H.grade; 
