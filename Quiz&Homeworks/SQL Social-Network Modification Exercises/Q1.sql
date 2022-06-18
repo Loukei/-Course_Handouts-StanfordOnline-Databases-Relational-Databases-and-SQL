42a78a5692d404ae50485608e2de97dbaa8b165d
Loukei <weber781015+bitbucket@gmail.com>
2022-06-18 12:55:12 +0800

    social network modify q1 q2(todo)

--- Quiz&Homeworks/SQL Social-Network Modification Exercises/Q1.sql
+++ Quiz&Homeworks/SQL Social-Network Modification Exercises/Q1.sql
@@ -8,3 +8,7 @@ It's time for the seniors to graduate. Remove all 12th graders from Highschooler
 ## ANS
 
 */
+
+DELETE 
+FROM Highschooler
+WHERE grade = 12;
--- Quiz&Homeworks/SQL Social-Network Modification Exercises/Q2.sql
+++ Quiz&Homeworks/SQL Social-Network Modification Exercises/Q2.sql
@@ -1,5 +1,25 @@
 /*
 ## Question
+If two students A and B are friends, 
+and A likes B but not vice-versa, 
+remove the Likes tuple.
+
+##
+
+--- A LIKE B BUT B NOT LIKE A
+SELECT *
+FROM Likes L1
+WHERE NOT EXISTS(
+	SELECT *
+	FROM Likes L2
+	WHERE L1.ID1 = L2.ID2 AND L1.ID2 = L2.ID1
+);
+
+SELECT L1.ID1, L1.ID2
+FROM Likes L1
+LEFT JOIN Likes L2
+ON L1.ID1 = L2.ID2 AND L1.ID2 = L2.ID1
+WHERE L2.ID1 IS NULL;
 
 */
 
@@ -8,3 +28,13 @@
 ## ANS
 
 */
+DELETE FROM Friend
+WHERE EXISTS(
+	SELECT *
+	FROM Likes L1
+	WHERE NOT EXISTS(
+		SELECT *
+		FROM Likes L2
+		WHERE L1.ID1 = L2.ID2 AND L1.ID2 = L2.ID1
+	) AND L1.ID1 = Friend.ID1 AND L1.ID2 = Friend.ID2
+);
