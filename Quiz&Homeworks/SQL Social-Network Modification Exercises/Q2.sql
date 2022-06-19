e187927628bd27216861b7f9e48abdc5b962a771
Loukei <weber781015+bitbucket@gmail.com>
2022-06-19 15:05:40 +0800

    SQL Social-Network Modification Exercises Q3 finished

--- Quiz&Homeworks/SQL Social-Network Modification Exercises/Q2.sql
+++ Quiz&Homeworks/SQL Social-Network Modification Exercises/Q2.sql
@@ -21,6 +21,12 @@ LEFT JOIN Likes L2
 ON L1.ID1 = L2.ID2 AND L1.ID2 = L2.ID1
 WHERE L2.ID1 IS NULL;
 
+SELECT * FROM Likes 
+EXCEPT 
+SELECT L2.ID2 AS 'ID1', L2.ID1 AS 'ID2' FROM Likes L2;
+
+[DELETE WITH INTERSECT](https://stackoverflow.com/questions/31014126/delete-with-intersect)
+
 */
 
 
@@ -38,3 +44,6 @@ WHERE EXISTS(
 		WHERE L1.ID1 = L2.ID2 AND L1.ID2 = L2.ID1
 	) AND L1.ID1 = Friend.ID1 AND L1.ID2 = Friend.ID2
 );
+
+1641 Brittany		1468 Kris	
+1689 Gabriel		1247 Alexis	
--- Quiz&Homeworks/SQL Social-Network Modification Exercises/Q3.sql
+++ Quiz&Homeworks/SQL Social-Network Modification Exercises/Q3.sql
@@ -1,10 +1,35 @@
 /*
 ## Question
 
+For all cases where A is friends with B, 
+and B is friends with C, 
+add a new friendship for the pair A and C.
+
+Do not add duplicate friendships, 
+friendships that already exist, or friendships with oneself. 
+(This one is a bit challenging; congratulations if you get it right.)
 */
 
 
 /*
 ## ANS
 
+--- find c for (a,b) and (b,c)
+SELECT F1.ID1 AS 'A', F1.ID2 AS 'B', F2.ID2 AS 'C'
+FROM Friend F1, Friend F2
+ON F1.ID2 = F2.ID1 AND F1.ID1 <> F2.ID2;
+
+-- remove those friend relations which is already exist
+SELECT F1.ID1 AS 'ID1', F2.ID2 AS 'ID2'
+FROM Friend F1, Friend F2
+ON F1.ID2 = F2.ID1 AND F1.ID1 <> F2.ID2
+EXCEPT
+SELECT * FROM Friend
 */
+
+INSERT INTO Friend (ID1,ID2)
+SELECT F1.ID1 AS 'ID1', F2.ID2 AS 'ID2'
+FROM Friend F1, Friend F2
+ON F1.ID2 = F2.ID1 AND F1.ID1 <> F2.ID2
+EXCEPT
+SELECT * FROM Friend;
