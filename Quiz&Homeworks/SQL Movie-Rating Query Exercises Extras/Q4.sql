3ef1973c6b97ae36441ea2d93229e494923d90dd
Loukei <weber781015+bitbucket@gmail.com>
2022-06-21 14:45:03 +0800

    Movie-Rating Query Exercises Extras Q4

--- Quiz&Homeworks/SQL Movie-Rating Query Exercises Extras/Q4.sql
+++ Quiz&Homeworks/SQL Movie-Rating Query Exercises Extras/Q4.sql
@@ -1,6 +1,6 @@
 /*
 ## Question
-
+Find the titles of all movies not reviewed by Chris Jackson.
 */
 
 
@@ -8,3 +8,14 @@
 ## ANS
 
 */
+SELECT title
+FROM Movie
+WHERE mID NOT IN (
+	SELECT mID
+	FROM Rating
+	WHERE rID = (
+		SELECT rID 
+		FROM Reviewer R 
+		WHERE name = "Chris Jackson"
+	)
+);
