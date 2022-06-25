52997bc1baefa2123236e1eacf09905d32588558
Loukei <weber781015+bitbucket@gmail.com>
2022-06-25 23:39:15 +0800

    SQL Movie-Rating Query Exercises Extras - Q7

--- Quiz&Homeworks/SQL Movie-Rating Query Exercises Extras/Q7.sql
+++ Quiz&Homeworks/SQL Movie-Rating Query Exercises Extras/Q7.sql
@@ -1,6 +1,8 @@
 /*
 ## Question
 
+List movie titles and average ratings, from highest-rated to lowest-rated. 
+If two or more movies have the same average rating, list them in alphabetical order.
 */
 
 
@@ -8,3 +10,12 @@
 ## ANS
 
 */
+
+SELECT Movie.title, AVG_rate
+FROM (
+    --- calculate AVG(stars) for each movie
+	SELECT mID, AVG(stars) AS 'AVG_rate'
+	FROM Rating
+	GROUP BY mID
+) LEFT JOIN Movie USING(mID)
+ORDER BY AVG_rate DESC, Movie.title ASC;
