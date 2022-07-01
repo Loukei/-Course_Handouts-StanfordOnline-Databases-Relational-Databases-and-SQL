1b2c49b4e497dd65e4158ba2621611b568a48b5f
Loukei <weber781015+bitbucket@gmail.com>
2022-07-01 11:18:32 +0800

    SQL Social-Network Query Exercises Extras - Q1

--- Quiz&Homeworks/SQL Social-Network Query Exercises Extras/Q1.sql
+++ Quiz&Homeworks/SQL Social-Network Query Exercises Extras/Q1.sql
@@ -1,6 +1,8 @@
 /*
 ## Question
-
+For every situation where student A likes student B, 
+but student B likes a different student C, 
+return the names and grades of A, B, and C.
 */
 
 
@@ -8,3 +10,13 @@
 ## ANS
 
 */
+SELECT H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+FROM (
+	SELECT L1.ID1 AS 'A', L1.ID2 AS 'B', L2.ID2 AS 'C'
+	FROM Likes L1
+	INNER JOIN Likes L2
+	ON L1.ID2 = L2.ID1 AND L1.ID1 != L2.ID2
+) AS T
+INNER JOIN Highschooler H1 ON T.A = H1.ID
+INNER JOIN Highschooler H2 ON T.B = H2.ID
+INNER JOIN Highschooler H3 ON T.C = H3.ID;
