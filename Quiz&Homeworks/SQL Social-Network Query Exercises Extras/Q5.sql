a753914cfd1ab83a3f7ff8a14f6aeb453bcb4002
Loukei <weber781015+bitbucket@gmail.com>
2022-07-05 11:50:56 +0800

    SQL Social-Network Query Exercises Extras - Q5

--- Quiz&Homeworks/SQL Social-Network Query Exercises Extras/Q5.sql
+++ Quiz&Homeworks/SQL Social-Network Query Exercises Extras/Q5.sql
@@ -1,6 +1,7 @@
 /*
 ## Question
 
+Find the name and grade of the student(s) with the greatest number of friends.
 */
 
 
@@ -8,3 +9,14 @@
 ## ANS
 
 */
+WITH cte AS
+(
+	SELECT ID1, count(ID2) as 'count_n'
+	FROM Friend
+	GROUP BY ID1
+)
+SELECT H.name, H.grade
+FROM cte
+INNER JOIN Highschooler H
+ON H.ID = cte.ID1
+WHERE count_n = ( SELECT max(count_n) FROM cte);
